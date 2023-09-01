036c7215fd83f4054aba3c6d3983722b573e4d9c
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-09-01 11:32:17 +0200

    Mark Order As Delivered

--- base/views/order_views.py
+++ base/views/order_views.py
@@ -114,3 +114,17 @@ def updateOrderToPaid(request, pk):
 
 
     return Response('Order was paid')
+
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateOrderToDelivered(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isDelivered = True
+    order.deliveredAt = datetime.now()
+    order.save()
+
+
+    return Response('Order was delivered')
